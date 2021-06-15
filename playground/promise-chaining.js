e5ecd4acc0adb9bf6fbd522cf7f2bbeec8929df2
krish5512 <krk5512@gmail.com>
2021-06-15 22:11:54 +0530

    asyncawait code added

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,12 +1,37 @@
 require('../src/db/mongoose');
 const User = require('../src/models/user');
 
+// Async Await method
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {
+        age
+    });
+    const count = await user.countDocuments({
+        age
+    });
+    return count;
+}
 
-User.findByIdAndUpdate('60c48b0a445fa274b87aaf7d',{age : 1}).then((user) => {
-    console.log(user);
-    return User.countDocuments({ age : 1});
-}).then((result) => {
-    console.log({result});
+updateAgeAndCount('60c48b0a445fa274b87aaf7d',23).then((result) => {
+    console.log({result})
 }).catch((e) => {
     console.log({e})
 })
+
+// Promise Chaining Method
+// User.findByIdAndUpdate('60c48b0a445fa274b87aaf7d', {
+//     age: 1
+// }).then((user) => {
+//     console.log(user);
+//     return User.countDocuments({
+//         age: 1
+//     });
+// }).then((result) => {
+//     console.log({
+//         result
+//     });
+// }).catch((e) => {
+//     console.log({
+//         e
+//     })
+// })
--- playground/promise-chaining2.js
+++ playground/promise-chaining2.js
@@ -1,17 +1,34 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('60c59ee4dbdf9e4f1cbc8b30').then((task) => {
-    console.log(task);
-    return Task.countDocuments({
+// Async Await method 
+
+const taskCheckMethod = async (_id) => {
+    await Task.findByIdAndDelete(_id);
+    const taskCount = await Task.countDocuments({
         completed: false
-    })
-}).then((result) => {
-    console.log({
-        result
     });
-}).catch((e) => {
-    console.log({
-        e
-    })
+    return taskCount;
+}
+
+taskCheckMethod('60c59ee4dbdf9e4f1cbc8b30').then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error)
 })
+
+// Promise Chaining method
+// Task.findByIdAndDelete('60c59ee4dbdf9e4f1cbc8b30').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({
+//         completed: false
+//     })
+// }).then((result) => {
+//     console.log({
+//         result
+//     });
+// }).catch((e) => {
+//     console.log({
+//         e
+//     })
+// })
