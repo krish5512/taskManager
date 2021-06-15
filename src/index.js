3adb3c60195d440b0e7babaa35cce21f562f9fec
krish5512 <krk5512@gmail.com>
2021-06-15 14:08:37 +0530

    read single task and multiple Tasks

--- src/index.js
+++ src/index.js
@@ -21,10 +21,18 @@ app.get('/users', (req, res) => {
   }).catch((e) => {
     res.status(500).send()
   })
-})
-app.get('users/:id',(req,res) => {
-  console.log(req.params)
-})
+});
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id).then((user) => {
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user)
+  }).catch((e) => {
+    res.status(500).send(e)
+  })
+});
 // methods written to write data to DB
 app.post('/users', (req, res) => {
   const user = new User(req.body);
@@ -34,6 +42,33 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 })
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((tasks) => {
+    res.send(tasks);
+  }).catch((e) => {
+    res.status(500).send(e);
+  })
+})
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id).then((task) => {
+    if (!task) {
+      return res.status(404).send()
+    }
+    res.send(task)
+  }).catch((e) => {
+    res.status(500).send(e)
+  })
+})
+
+
+
+
+
+
+
+
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save().then(() => {
