8f6c1bb1d11191cd5a84bc1ae7ec4b10587bbd49
krish5512 <krk5512@gmail.com>
2021-06-24 13:48:07 +0530

    basic UI Text designed

--- tm_frontend/src/App.js
+++ tm_frontend/src/App.js
@@ -1,13 +1,51 @@
 import './App.css';
+import {
+  useState
+} from 'react';
+
 
 function App() {
+
+  const [signInDiv, setSignInDiv] = useState(false);
+  const [signUpDiv, setSignUpDiv] = useState(false);
+
+  const handleSign = e => {
+    if (e === 'in') {
+      setSignInDiv(true);
+      setSignUpDiv(false);
+    } else {
+      setSignInDiv(false);
+      setSignUpDiv(true);
+    }
+  }
   return (
     <div className="App">     
-    <p>Hi USer this is React UI</p>
-      <form action="../../post" method="post" 
-      className="form">
-  <button type="submit">Connected?</button>
-</form>
+    <h1>Task Manager</h1>
+    <h5>Add your tasks for ease.</h5>    
+  <button onClick={() => 
+    handleSign('in')}>Sign In</button>
+  <button onClick={() => 
+    handleSign('up')}>Sign Up</button>
+    {
+      signInDiv && 
+      <div>
+      <div>Sign In</div>
+      <p>Email ID : </p>
+      <p>Password : </p>
+      <button>Log In</button>
+      </div>
+    }
+    {
+      signUpDiv && 
+      <div>
+      <div>Sign Up</div>
+      <p>Name : </p>
+      <p>Email ID :</p>
+      <p>Password : </p>
+      <p>Age : </p>
+      <button>Create my account</button>
+      </div>
+    }
     </div>
   );
 }
